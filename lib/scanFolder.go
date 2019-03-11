48e14b988a62c1f3d479e10d3e66b28578d29a82
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-11 16:10:37 -0700

    Added GetFileChangedTime

--- lib/scanFolder.go
+++ lib/scanFolder.go
@@ -27,7 +27,7 @@ type File struct {
 
 // ScanFolder scans the given directory and
 // returns a slice containing all the files
-func ScanFolder() []*File {
+func ScanFolder() ([]*File, error) {
 
 	// initialize empty slice to hold file data
 	folderFiles := make([]*File, 0)
@@ -40,7 +40,7 @@ func ScanFolder() []*File {
 	// check for potensial errors while reading
 	files, err := filepath.Glob(dir + pattern)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	// iterate over files in folder
@@ -48,12 +48,13 @@ func ScanFolder() []*File {
 
 		// add new File struct with file properties
 		folderFiles = append(folderFiles, &File{
-			name:      FilenameWithoutExt(file),
-			extension: filepath.Ext(file),
-			folder:    filepath.Dir(file),
-			path:      FullAbsPath(file),
-			size:      GetFileSize(file),
-			fileNr:    i + 1,
+			name:        FilenameWithoutExt(file),
+			extension:   filepath.Ext(file),
+			folder:      filepath.Dir(file),
+			path:        FullAbsPath(file),
+			size:        GetFileSize(file),
+			lastChanged: GetFileChangedTime(file),
+			fileNr:      i + 1,
 		})
 	}
 
@@ -61,7 +62,7 @@ func ScanFolder() []*File {
 		fmt.Println(file)
 	}
 
-	return folderFiles
+	return folderFiles, nil
 }
 
 // FilenameWithoutExt returns the cleaned filename
@@ -101,4 +102,21 @@ func GetFileSize(file string) string {
 
 	// return the file size from stats
 	return strconv.Itoa(int(stats.Size()))
+}
+
+// GetFileChangedTime returns the timestamp
+// of the passed in files last changed time
+// if error, return 'N/A'
+func GetFileChangedTime(file string) string {
+
+	// get stats from passed in file
+	stats, err := os.Stat(file)
+
+	// handle potensial errors
+	if err != nil {
+		return "N/A"
+	}
+
+	// return the modified time as string
+	return stats.ModTime().String()
 }
